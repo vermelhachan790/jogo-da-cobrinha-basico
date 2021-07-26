974f56c0c626175e47c9e3e56519e9c175f033c5
vermelhachan790 <83366923+vermelhachan790@users.noreply.github.com>
2021-07-26 10:41:10 -0300

    Update Jogo.c

--- Jogo.c
+++ Jogo.c
@@ -1,11 +1,11 @@
-#include <conio.h>
+ #include <conio.h>
 #include <stdio.h>
 #include <ctype.h>
 
 int vida = 3;
 int score = 0;
 float h;
-int contado=0;
+int contado = 0;
 int key;
 char key1;
 char x = 20;
@@ -16,10 +16,10 @@ float j;
 
 int main()
 {
-	char food= '@';
-int fodPos[4];
-fodPos[0] = 10;
-fodPos[1] = 40;
+	char food = '@';
+	int fodPos[4];
+	fodPos[0] = 10;
+	fodPos[1] = 40;
 	char inimigo = '-';
 	int map[10];
 	map[0] = 0;
@@ -46,95 +46,111 @@ fodPos[1] = 40;
 	while ((key = getch()) != 'q')
 	{
 		clrscr();
-		for (int i =0; i<20; i++)
+		for (int i = 0; i < 20; i++)
 		{
-		 j = j + 1;
-        gotoxy(map[2], map[3]);
-        printf("____________________________________________________________");
-        gotoxy(map[0], map[1]);
-        printf("____________________________________________________________");
-        
-        }
-        gotoxy(20, 20);
-        textcolor(LIGHTGREEN);
-        printf("Score:%i",score);
-        textcolor(WHITE);
-        gotoxy(fodPos[1], fodPos[0]);
-        printf("%c",food);
-        textcolor(BLUE);
-         
+			j = j + 1;
+			gotoxy(map[2], map[3]);
+			printf("____________________________________________________________");
+			gotoxy(map[0], map[1]);
+			printf("____________________________________________________________");
+		}
+		gotoxy(20, 20);
+		textcolor(LIGHTGREEN);
+		printf("Score:%i", score);
+		textcolor(WHITE);
+		gotoxy(fodPos[1], fodPos[0]);
+		printf("%c", food);
+		textcolor(BLUE);
+
 		gotoxy(0, 15);
 		printf("Aperta d para direita, w pra cima, v pra baixo, s pra esquerda");
 		gotoxy(x, y);
 		int vdd; //one simple animator
-	 if (y == map[0])
+		if (y == map[0])
 		{
-		
-		   while((key == getch())!='w')
-		   {
-		   	clrscr();
-		   
-		   	printf("Voce perdeu");
-		   }
+			while ((key == getch()) != 'w')
+			{
+				clrscr();
+
+				printf("Voce perdeu");
+			}
 		}
-			 if (y == map[3])
+		if (y == map[3])
 		{
-		
-		   while((key == getch())!='w')
-		   {
-		   	clrscr();
-		   
-		   	printf("Voce perdeu");
-		   }
+			while ((key == getch()) != 'w')
+			{
+				clrscr();
+
+				printf("Voce perdeu");
+			}
 		}
 		if (x == fodPos[1])
 		{
-			
 			score = score + 1;
-		    contado = 4;
-		     if (contado == 4)
-		     {
-		     	fodPos[0] = fodPos[0] +1000;
-		     }
-		
+			contado = 4;
+			if (contado == 4)
+			{
+				if (score == 1)
+				{   
+				
+				
+					fodPos[0] = fodPos[0] -5;
+					fodPos[1] = fodPos[1] - 20;
+					
+				}
+				if (score == 2)
+				{
+					fodPos[0] = fodPos[0] +5;
+					fodPos[1] = fodPos[1] +20;
+				}
+			}
 		}
-		
-			
-		
+
 		if (vdd == 5)
 		{
+			textcolor(RED);
 			printf("<");
+			textcolor(BLUE);
 		}
 		if (vdd == 4)
-		{
+		{   textcolor(RED);
 			printf(">");
+			textcolor(BLUE);
 		}
 		if (vdd == 6)
 		{
-		
 		}
 		switch (key)
 		{
 		case 'd':
 			x++;
 			vdd = 4;
+			textcolor(RED);
 			printf(">");
+			textcolor(BLUE);
 
 			break;
 		case 's':
 			vdd = 5;
+			textcolor(RED);
 			printf("<");
+			textcolor(BLUE);
 			x--;
 			break;
-			case 'w':
-			 vdd = 6;
-			 printf("^");
-			 y--;
-			 break;
-			 case 'v':
-			  vdd = 7;
-			  printf("v");
-			  y++;
+		case 'w':
+			vdd = 6;
+			textcolor(RED);
+			printf("^");
+			textcolor(BLUE);
+			y--;
+			break;
+		case 'v':
+			vdd = 7;
+			textcolor(RED);
+			
+			printf("v");
+			textcolor(BLUE);
+			y++;
 		}
 	}
 }
